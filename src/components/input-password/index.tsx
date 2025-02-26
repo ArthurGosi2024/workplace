0cb2a037840abcfc055e617f83a220419a6c8e8a
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-02-26 00:24:31 -0300

    add: created password input with icon

---
+++ src/components/input-password/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {ComponentProps, useState} from "react";
+import {Icon} from "@iconify/react";
+
+
+
+
+interface  IInputPasswordPros extends  ComponentProps<'input'> {}
+
+export default  function InputPassword({...rest} : IInputPasswordPros) {
+
+    const [typeToogle,setTypeToogle] = useState<'text' | 'password'>('text')
+
+    const onTypeToogle = () => {
+        if (typeToogle === 'text') {
+            setTypeToogle('password')
+        }else {
+            setTypeToogle('text')
+        }
+
+    }
+
+    return <div className={'relative flex justify-center items-center h-full'}>
+        <input
+            type={typeToogle}
+            className="w-[403px] h-12 bg-white rounded-[14px] pr-12 outline-none  px-[18px]  shadow-[0px_1px_2px_0px_rgba(184,200,224,0.22)] border border-[#d8e0ef] text-[#7d8592] text-sm font-normal font-['Nunito Sans'] leading-normal"
+
+            {...rest}
+       />
+        <div className={'absolute right-4 cursor-pointer'} >
+            <Icon icon={ typeToogle === 'text' ? 'clarity:eye-hide-solid' : 'clarity:eye-show-line'} color={'#7d8592'} className={'transition-all'} onClick={onTypeToogle} />
+        </div>
+    </div>
+}
