bc772afa4e09ab69f4bb0c98e4175b33c1c15f7b
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-03-07 10:03:37 -0300

    chore: input components being reused

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { CheckboxWithText } from "@/components/checkbox-with-text";
 import { useActionState } from "react";
 import { handleLogin } from "./action";
+import Input from "@/components/input";
+import ErrorInput from "@/components/error-input";
 
 export default function SignIn() {
 	const [state, action] = useActionState(handleLogin, null);
@@ -19,33 +21,19 @@ export default function SignIn() {
 				className={"flex flex-col justify-center items-center gap-20"}
 			>
 				<div className="flex flex-col gap-[29px]">
-					<div className="flex flex-col gap-1.5 ">
-						<div className="text-[#7d8592] text-sm font-bold leading-normal">
-							Email
-						</div>
-						<input
-							type="email"
-							name="email"
-							className="w-[403px] h-12 bg-white rounded-[14px] outline-none  px-[18px]  shadow-[0px_1px_2px_0px_rgba(184,200,224,0.22)] border border-[#d8e0ef] text-[#7d8592] text-sm font-normal font-['Nunito Sans'] leading-normal"
-						/>
-						<div className="h-[14px]">
-							{state?.errors?.email && (
-								<p className="text-sm text-red-500">{state?.errors.email[0]}</p>
-							)}
-						</div>
-					</div>
+					<Input
+						label="Email"
+						type="email"
+						name="email"
+						errors={state?.errors?.email![0] ?? ""}
+						typeInput="email"
+					/>
 					<div className="flex flex-col gap-1.5 ">
 						<div className="text-[#7d8592] text-sm font-bold leading-normal">
 							Senha
 						</div>
 						<InputPassword name="password" />
-						<div className="h-[14px]">
-							{state?.errors?.password && (
-								<p className="text-sm text-red-500">
-									{state?.errors.password[0]}
-								</p>
-							)}
-						</div>
+						<ErrorInput message={state?.errors?.password![0] ?? ""} />
 					</div>
 
 					<div className={"flex justify-between"}>
---
+++ src/components/input/index.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from "react";
+import ErrorInput from "../error-input";
+
+type InputType = "email" | "password" | string;
+
+interface IInputProps extends ComponentProps<"input"> {
+	typeInput: InputType;
+	label: string;
+	errors: string;
+}
+
+export default function Input({
+	typeInput,
+	label,
+	errors,
+	...rest
+}: IInputProps) {
+	return (
+		<div className="flex flex-col gap-1.5 ">
+			<div className="text-[#7d8592] text-sm font-bold leading-normal">
+				{label}
+			</div>
+			<input
+				{...rest}
+				className="w-[403px] h-12 bg-white rounded-[14px] outline-none  px-[18px]  shadow-[0px_1px_2px_0px_rgba(184,200,224,0.22)] border border-[#d8e0ef] text-[#7d8592] text-sm font-normal font-['Nunito Sans'] leading-normal"
+			/>
+			<ErrorInput message={errors} />
+		</div>
+	);
+}
