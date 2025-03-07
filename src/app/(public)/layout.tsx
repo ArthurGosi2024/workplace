29d75d057fe7f3ddcf90cebae750ea04c284665a
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-03-07 10:02:41 -0300

    add: adding primereact context to use other components

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,6 +3,8 @@
 import { Nunito_Sans } from "next/font/google";
 import "../globals.css";
 import DefaultIlustration from "@/components/default-ilustration";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { usePathname } from "next/navigation";
 
 const nunitoSans = Nunito_Sans({
@@ -18,21 +20,24 @@ export default function RootLayout({
 }>) {
 	const pathname = usePathname();
 
-
-	console.log(pathname === "/sign-up")
 	return (
-		<html lang="en">
-			<body className={nunitoSans.className}>
-				<div className="bg-[#f4f9fd] antialiased  w-screen h-screen px-10 pt-5 pb-[30px]">
-					<div
-						className={`flex  w-full h-full ${pathname === "/sign-up" ? "gap-8" : "gap-0"
+		<PrimeReactProvider>
+			<html lang="en">
+				<body className={nunitoSans.className}>
+					<div className="bg-[#f4f9fd] antialiased  w-screen h-screen px-10 pt-5 pb-[30px]">
+						<div
+							className={`flex  w-full h-full ${
+								pathname === "/sign-up" ? "gap-8" : "gap-0"
 							} transition-all  rounded-3xl overflow-hidden  `}
-					>
-						<DefaultIlustration pathname={pathname} />
-						<div className="flex-1/2 h-full bg-white rounded-3xl p-5">{children}</div>
+						>
+							<DefaultIlustration pathname={pathname} />
+							<div className="flex-1/2 h-full bg-white rounded-3xl p-5">
+								{children}
+							</div>
+						</div>
 					</div>
-				</div>
-			</body>
-		</html>
+				</body>
+			</html>
+		</PrimeReactProvider>
 	);
 }
