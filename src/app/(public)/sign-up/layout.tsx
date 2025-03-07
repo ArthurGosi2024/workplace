15fbe902d991bdb638b6fa764fbe615407623bac
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-03-07 10:04:58 -0300

    feat: creating the sign-up layout

--- src/app/(public)/sign-up/layout.tsx
+++ src/app/(public)/sign-up/layout.tsx
@@ -1,6 +1,29 @@
+import ButtonRedirect from "@/components/button-redirect";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Get started",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <div className="flex justify-center items-center p-14">
-        h-full
-        {children}
-    </div>
+	return (
+		<div className="flex justify-between items-center flex-col gap-[6px]  h-full">
+			<header className="relative text-center justify-start text-[#3f8cff] text-sm font-bold uppercase">
+				Step 1/4
+			</header>
+			<main className="flex-1 border-b border-[#e4e6e8] w-full">
+				{children}
+			</main>
+			<footer className="flex justify-between items-center h-[84px] w-full">
+				<ButtonRedirect
+					label="Anterior"
+					bgTransparent={true}
+					icon="formkit:arrowleft"
+					shandow={false}
+					reverse={true}
+				/>
+				<ButtonRedirect label="Proximo" icon="formkit:arrowright" />
+			</footer>
+		</div>
+	);
 }
